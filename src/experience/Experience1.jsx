5a5a49f10e06d2a25217109cc7945f925f8f26a4
Erik <eriktruckner@comcast.net>
2023-08-24 08:10:18 -0400

    xp working

--- src/experience/Experience1.jsx
+++ src/experience/Experience1.jsx
@@ -3,6 +3,7 @@ import { Canvas } from '@react-three/fiber';
 export const Experience1 = () => {
   return (
     <>
+      <h1>Spicy</h1>
       <Canvas>
         <color attach='background' args={['lightblue']} />
       </Canvas>
