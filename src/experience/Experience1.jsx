170a015b623cab42f9f971fb6654098711629099
Erik <eriktruckner@comcast.net>
2023-09-04 14:15:06 -0400

    canvas and html set up

--- src/experience/Experience1.jsx
+++ src/experience/Experience1.jsx
@@ -3,7 +3,9 @@ import { Canvas } from '@react-three/fiber';
 export const Experience1 = () => {
   return (
     <>
-      <h1 className='absolute z-10'>Spicy </h1>
+      <div className='absolute z-10'>
+        <h1>Spicy </h1>
+      </div>
       <Canvas>
         <color attach='background' args={['lightblue']} />
       </Canvas>
